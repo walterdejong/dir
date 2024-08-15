2e4a049e2960ae2a70066a88d796339ab79ca9ee
Walter de Jong <walter@heiho.net>
2024-08-15 13:11:53 +0200

    yeah

---
+++ src/entry.rs
@@ -0,0 +1,58 @@
+//
+//  dir     WJ124
+//  entry.rs
+//
+
+use std::fs;
+use std::fs::{DirEntry, Metadata};
+use std::ffi::OsString;
+use std::path::PathBuf;
+use std::io;
+use chrono::{DateTime, Local, TimeZone};
+
+pub struct Entry {
+    pub name: OsString,
+    pub metadata: Metadata,
+    pub link_dest: Option<PathBuf>,
+}
+
+impl Entry {
+    pub fn from_dir_entry(d: &DirEntry) -> Result<Entry, io::Error> {
+        let path = d.path();
+        let some_filename = path.file_name();
+        if some_filename.is_none() {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid filename"));
+        }
+        let filename = some_filename.unwrap().to_os_string();
+
+        let metadata = d.metadata()?;
+        let link_dest = if metadata.is_symlink() {
+            Some(fs::read_link(path)?)
+        } else {
+            None
+        };
+
+        Ok(Entry {
+            name: filename,
+            metadata,
+            link_dest,
+        })
+    }
+
+    pub fn mtime(&self) -> DateTime<Local> {
+        if let Ok(t) = self.metadata.modified() {
+            t.into()
+        } else {
+            Local.timestamp_opt(0, 0).unwrap()
+        }
+    }
+
+    pub fn is_hidden(&self) -> bool {
+        // sucks that we have to convert this entire thing just to look at one first character
+        let s = self.name.to_string_lossy();
+        let first = s.chars().next().expect("panic: this should not have happened");
+        first == '.'
+    }
+}
+
+// EOB
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,58 @@
 //  main.rs
 //
 
-use std::fs;
-use chrono::{DateTime,Local};
+pub mod entry;
+
+use entry::Entry;
+use std::{cmp::Ordering, fs};
+use chrono::{DateTime, Local, Datelike};
+use lazy_static::lazy_static;
+
+lazy_static!(
+    static ref CURRENT_YEAR: i32 = chrono::Local::now().year();
+);
+
+fn format_time(dt: &DateTime<Local>) -> String {
+    if dt.year() == *CURRENT_YEAR {
+        format!("{}", dt.format("%b %d %H:%M"))
+    } else {
+        format!("{}", dt.format("%b %d  %Y"))
+    }
+}
+
+fn format_entry(entry: &Entry) -> String {
+    let time_str = format_time(&entry.mtime());
+
+    let size_str;
+    if entry.metadata.is_dir() {
+        size_str = format!("{:^16}", "<DIR>");
+    } else {
+        size_str = format!("{:>16}", entry.metadata.len());
+    }
+
+    let display_name = entry.name.to_string_lossy();
+
+    let mut buf = format!("{}  {}  {}", &time_str, &size_str, &display_name);
+
+    if entry.metadata.is_dir() {
+        buf.push('/');
+    }
+
+    if entry.metadata.is_symlink() {
+        if let Some(linkdest_path) = &entry.link_dest {
+            let display_linkdest = linkdest_path.to_string_lossy();
+            buf.push_str(&format!(" -> {}", &display_linkdest));
+        }
+        // else: should not / can not happen, just ignore it
+    }
+
+    buf
+}
 
 fn main() {
-    let dir = fs::read_dir("/").expect("error reading directory");
+    let dir = fs::read_dir("/home/walter").expect("error reading directory");
+
+    let mut entries = Vec::new();
 
     for dir_entry in dir {
         // dbg!(&dir_entry);
@@ -19,58 +66,49 @@ fn main() {
             }
         };
 
-        let path = entry.path();
+        // an fs::DirEntry holds an open file descriptor to the directory
+        // we don't want that ... so therefore I convert it to a custom Entry type
+        // the Entry holds all the same attributes; name, metadata, linkdest (if it is a symbolic link)
+        // but also (attempts) has an easier interface
+        // Mind that the conversion may error, in which case we print the error
+        // and skip this entry
 
-        let metadata = match entry.metadata() {
+        let entry = match Entry::from_dir_entry(&entry) {
             Ok(v) => v,
             Err(e) => {
-                eprintln!("error: {}: {}", path.to_string_lossy(), e);
+                eprintln!("error: {}", e);
                 continue;
             }
         };
 
-        if let Ok(mtime) = metadata.modified() {
-            let dt: DateTime<Local> = mtime.into();
-            let s_mtime = dt.format("%Y-%m-%d %H:%M");
-            print!("{}  ", &s_mtime);
-        } else {
-            // mtime not supported on platform
-            print!("                  ");
-        }
-
-        if metadata.is_dir() {
-            print!("<DIR>  ");
-        } else {
-            print!("       ");
-        }
-
-        let size = metadata.len();
-        print!("{:>10}  ", size);
-
-        if let Some(name) = path.file_name() {
-            print!("{}", name.to_string_lossy());
-
-            if metadata.is_dir() {
-                print!("/");
-            }
-
-            if metadata.is_symlink() {
-                print!(" -> ");
-                let link_dest = match fs::read_link(path) {
-                    Ok(v) => v,
-                    Err(e) => {
-                        eprintln!("error: {}", e);
-                        continue;
-                    }
-                };
-                print!("{}", link_dest.to_string_lossy());
-            }
-
-        } else {
-            eprintln!("error: failed to get filename from path");
+        if entry.is_hidden() {
             continue;
         }
-        println!("");
+
+        entries.push(entry);
+    }
+
+    entries.sort_by(sort_dirs_first);
+
+    for entry in entries.iter() {
+        println!("{}", format_entry(entry));
+    }
+}
+
+fn sort_dirs_first(a: &Entry, b: &Entry) -> Ordering {
+    if a.metadata.is_dir() {
+        if b.metadata.is_dir() {
+            a.name.cmp(&b.name)
+        } else {
+            Ordering::Less
+        }
+    } else {
+        // a is a file or something else, but not a directory
+        if b.metadata.is_dir() {
+            Ordering::Greater
+        } else {
+            a.name.cmp(&b.name)
+        }
     }
 }
 
